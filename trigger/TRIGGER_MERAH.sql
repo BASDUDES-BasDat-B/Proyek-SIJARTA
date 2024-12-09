f0c28186c066c808a90e526405657506ff94df61
KrisnaPutraP <krisna.putra@ui.ac.id>
2024-12-09 22:15:19 +0700

    Trigger

--- trigger/TRIGGER_MERAH.sql
+++ trigger/TRIGGER_MERAH.sql
@@ -7,11 +7,11 @@ DECLARE
     v_id_pekerja UUID;
     v_kategori_tr UUID;
 BEGIN
-    IF NEW.status = 'Pesanan selesai' THEN  -- Lanjutkan jika status adalah "Pesanan selesai"
+    IF NEW.status = 'Pemesanan selesai' THEN  -- Lanjutkan jika status adalah "Pesanan selesai"
     
         -- Select total biaya dan id pekerja dari TR_PEMESANAN_JASA dan masukkan ke variabel total biaya dan id pekerja
         SELECT TR_J.TotalBiaya, TR_J.IdPekerja INTO v_total_biaya, v_id_pekerja
-        FROM STATUS_PESANAN AS S
+        FROM STATUS_PEMESANAN AS S
         LEFT JOIN TR_PEMESANAN_STATUS AS TR_S
             ON S.Id = TR_S.IdStatus
         LEFT JOIN TR_PEMESANAN_JASA AS TR_J
@@ -21,14 +21,14 @@ BEGIN
         -- Select id kategori transaksi dari KATEGORI_TR_MYPAY dan masukkan ke variabel kategori transaksi
         SELECT Id INTO v_kategori_tr
         FROM KATEGORI_TR_MYPAY
-        WHERE Nama = 'menerima honor transaksi jasa';
+        WHERE Nama = 'Menerima Honor';
 
         -- Masukkan transaksi ke TR_MYPAY
         INSERT INTO TR_MYPAY (Id, UserId, Tgl, Nominal, KategoriId)
         VALUES (gen_random_uuid(), v_id_pekerja, CURRENT_DATE, v_total_biaya, v_kategori_tr);
 
         -- Update saldo MyPay pekerja
-        UPDATE USER
+        UPDATE "USER"
         SET SaldoMyPay = SaldoMyPay + v_total_biaya
         WHERE Id = v_id_pekerja;
     END IF;
@@ -39,6 +39,6 @@ $$ LANGUAGE plpgsql;
 
 -- Membuat trigger ketika adanya update status pesanan dengan mengeksekusi Stored Procedure
 CREATE TRIGGER trg_status_update
-AFTER INSERT ON STATUS_PESANAN
+AFTER INSERT ON STATUS_PEMESANAN
 FOR EACH ROW
 EXECUTE FUNCTION proses_pembayaran_pekerja();
